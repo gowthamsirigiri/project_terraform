c9a2e1a1633eb02a94a22da968b6dcbf91a7eb9e
Gowtham Sirigiri <93024646+gowthamsirigiri@users.noreply.github.com>
2026-05-05 18:11:44 +0530

    Update and rename Day2_learn to Day2_learn.tf

---
+++ Day2_learn.tf
@@ -0,0 +1,18 @@
+#basic Vpc creation main.tf file
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.43.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example1" {
+  cidr_block = "10.0.0.0/16"
+}
